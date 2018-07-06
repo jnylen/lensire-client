7037973d9ea54b735a622a4675d99db50affe84f
Joakim Nylén <me@jnylen.nu>
2018-07-06 23:20:39 +0200

    Add Swedish

---
+++ src/langs/sv.js
@@ -0,0 +1,26 @@
+const sv = {
+  'waiting_on_products': 'Väntar på produkter..',
+  'header.menu.home': 'Hem',
+  'header.menu.daily': 'Endagslinser',
+  'header.menu.monthly': 'Månadslinser',
+  'header.menu.three_months': '3 Månadslinser',
+  'header.menu.weekly': 'Veckolinser',
+  'header.menu.biweekly': 'Varannan vecka',
+  'header.menu.yearly': 'Årslinser',
+  'homepage.info': 'Lensire är en webbplats för jämförelse av kontaktlinser som sammanställer priser över hela världen och försöker ge dig den bästa prissättningen för alla återförsäljare som kan leverera till dig.',
+  'homepage.chosen': 'Ditt valda land är <strong>{country}</strong> och valutan <strong>{currency}</strong>',
+  'homepage.popular_header': 'Populära kontaktlinser',
+  'products.info': 'Här är en lista för {type} kontaktlinser som vi just nu har priser för.',
+  'footer.by': '<strong>Lensire</strong> av <strong>Meiku</strong>',
+  'footer.affiliate': 'Länkar markerade som "Affiliate" är affiliate länkar och ger oss pengar tillbaka ifall att du köper därifrån.',
+  'footer.general': 'Allmänt',
+  'footer.general.impressum': 'Impressum',
+  'footer.general.about': 'Om oss',
+  'footer.general.contact': 'Kontakta oss',
+  'footer.legal': 'Legal',
+  'footer.legal.terms': 'Villkor',
+  'footer.legal.privacy_policy': 'Integritetspolicy',
+  'footer.legal.copyright': 'Upphovsrätt'
+}
+
+export default sv
