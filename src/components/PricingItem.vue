da97876388aa490f8c64fbe1dcc217c742a89ac5
Joakim Nylén <me@jnylen.nu>
2018-07-13 05:10:19 +0200

    Add flags to show in pricing list and header

--- src/components/PricingItem.vue
+++ src/components/PricingItem.vue
@@ -21,11 +21,14 @@
 
     <!-- total amount -->
     <div class="w-1/3 pt-2">
-      <div class="float-right mt-2">
+      <div class="float-right mt-1">
         <a class="bg-green-light py-2 px-8 text-white rounded-full text-sm" v-if="data.in_stock" :href="data.link" rel="nofollow" target="_blank">Köp</a>
         <a class="bg-grey py-2 px-8 text-white rounded-full text-sm" v-if="!data.in_stock" :href="data.link" rel="nofollow" target="_blank">Köp</a>
       </div>
       <div class="ml-10">
+        <div class="float-right mr-4" v-if="data.store.based_in_country !== getCountry">
+          <img class="h-6 w-6" :src="countryFlag(data.store.based_in_country)" />
+        </div>
         <h1 class="text-grey-darker font-semibold text-sm">{{ currencyToHuman(rounded(data.pricing_converted.shipping_prices.per_box, 2), data.pricing_converted.currency) }}</h1>
         <h5 class="text-grey-darker font-normal text-sm" v-if="data.pricing_converted.shipping_prices.per_box == data.pricing_converted.amount">{{ $t('pricing.shipping.free') }}</h5>
         <h5 class="text-grey-darker font-normal text-sm" v-if="data.pricing_converted.shipping_prices.per_box != data.pricing_converted.amount">{{ $t('pricing.shipping.cost', {text: currencyToHuman(rounded(data.pricing_converted.shipping_prices.amount, 2), data.pricing_converted.currency) }) }}</h5>
@@ -38,10 +41,16 @@
 <script>
 
 import { format } from 'currency-formatter'
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'PricingItem',
   props: ['data'],
+  computed: {
+    ...mapGetters([
+      'getCountry'
+    ]),
+  },
   methods: {
     rounded: function (value, decimals) {
       if (!value) {
@@ -57,6 +66,9 @@ export default {
     },
     currencyToHuman: function (value, currency) {
       return format(value, { code: currency })
+    },
+    countryFlag (value) {
+      return require('@/assets/flags/' + value.toLowerCase() + '.svg')
     }
   }
 }
