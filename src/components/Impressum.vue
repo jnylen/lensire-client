d79653963a8de1074229ecab2bdc06b27486f997
Joakim Nylén <me@jnylen.nu>
2018-07-13 09:46:02 +0200

    Update impressum with better disclaimers

--- src/components/Impressum.vue
+++ src/components/Impressum.vue
@@ -11,28 +11,66 @@ export default {
 </script>
 
 <template>
-<div class="container p-10 text-grey-darkest">
-    <h1>Meiku Kommanditbolag</h1>
-    <p>
+<div class="container p-10 pt-5 text-grey-darkest">
+    <h1 class="py-5">Meiku Kommanditbolag</h1>
+    <p class="leading-normal">
+        Lensire.com is run by Meiku Kommanditbolag, based out of Sweden.
+    </p>
+    <p class="leading-normal pt-5">
         Adress: Hagstigen 19, 14945 Nynäshamn, Sweden<br/>
         Email: contact@meiku.se<br/>
         Company Number: 969784-4158 (Sweden)<br/>
         VAT Number: SE969784415801
     </p>
-    <br/><br/><br/>
-    <h2>Disclaimer</h2>
-    <p class="pt-4">
-        <strong>Liability for Content</strong><br/>
-        We can’t take any guarantee for the accuracy, completeness and timeliness of the content. As a service provider we are responsible for the content of these pages according to § 7 paragraph 1 of the TMG. According to §§ 8 to 10 of the TMG we as service providers are not obliged to monitor transmitted or stored information or to investigate circumstances that indicate illegal activity. Responsibility for the removal or blocking of the use of information under general legal provisions is unaffected. A liability is only possible from the date of knowledge of a specific infringement. Upon notification of such violations, we will remove the content immediately.<br/><br/>
-        <strong>Liability for Links</strong><br/>
-        Our site contains links to external websites over which we have no control; therefore we cannot accept any liability for these websites. The respective provider or operator is responsible for the content of the linked sites. The linked sites were checked at the time of linking for possible violations, and illegal content was not present at the time the link was set on our site. A permanent control of the linked pages is unreasonable without concrete evidence of a violation of law. Upon notification of violations, we will immediately remove such links.<br/><br/>
-        <strong>Copyright</strong><br/>
-        The images, names and similar information about the contact lenses are owned by the manufacturer. We do not claim ownership over those items.<br/><br/>
-        <strong>Affiliate</strong><br/>
-        Links that goes from our website to webshops can be affiliate links, which means we get a percentage of the money you spend there. You can see if they are affiliate by looking at the url. If it's a direct link to the website (without going through a other website) it is not an affiliate link.<br/><br/>
-        <strong>Icons</strong><br/>
-        Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a>, <a href="https://www.flaticon.com/authors/mynamepong" title="mynamepong">mynamepong</a> and <a href="https://www.flaticon.com/authors/gregor-cresnar" title="Gregor Cresnar">Gregor Cresnar</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>.
-    </p>
+
+    <h1 class="pt-10">Disclaimer</h1>
+
+    <p class="py-5 font-medium">Last updated: July 13, 2018</p>
+
+    <p class="leading-normal">The information contained on https://lensire.com website (the "Service") is for general information purposes only.</p>
+
+    <p class="leading-normal">Meiku Kommanditbolag assumes no responsibility for errors or omissions in the contents on the Service.</p>
+
+    <p class="leading-normal">In no event shall Meiku Kommanditbolag be liable for any special, direct, indirect, consequential, or incidental damages or any damages whatsoever, whether in an action of contract, negligence or other tort, arising out of or in connection with the use of the Service or the contents of the Service. Meiku Kommanditbolag reserves the right to make additions, deletions, or modification to the contents on the Service at any time without prior notice.</p>
+
+    <p class="leading-normal">Meiku Kommanditbolag does not warrant that the Service is free of viruses or other harmful components.</p>
+
+
+    <h2 class="py-5">Affiliate disclaimer</h2>
+
+    <p class="leading-normal">This affiliate disclosure details the affiliate relationships of Meiku Kommanditbolag with other companies and products.</p>
+
+    <p class="leading-normal">Some of the links are "affiliate links", a link with a special tracking code. This means if you click on an affiliate link and purchase the item, we will receive an affiliate commission.</p>
+
+    <p class="leading-normal">The price of the item is the same whether it is an affiliate link or not. Regardless, we only recommend products or services we believe will add value to our readers.</p>
+
+    <p class="leading-normal">By using the affiliate links, you are helping support the Service, and we genuinely appreciate your support.</p>
+
+        
+    <p class="pt-5 leading-normal font-semibold">Affiliate advertising programs that the Service uses are:</p>
+
+    <ul>
+        <li>
+            <p class="leading-normal">Commission Junction</p>
+        </li>
+        <li>
+            <p class="leading-normal">Double.net</p>
+        </li>
+    </ul>
+
+
+    <h2 class="py-5">External links disclaimer</h2>
+
+    <p class="leading-normal">https://lensire.com website may contain links to external websites that are not provided or maintained by or in any way affiliated with Meiku Kommanditbolag</p>
+
+    <p class="leading-normal">Please note that the Meiku Kommanditbolag does not guarantee the accuracy, relevance, timeliness, or completeness of any information on these external websites.</p>
+
+    <h2 class="py-5">Copyright</h2>
+
+    <p class="leading-normal">The images, names and similar information about the contact lenses are owned by the manufacturer. We do not claim ownership over those items.</p>
+
+    <h2 class="py-5">Icons</h2>
+    <p class="leading-normal">Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a>, <a href="https://www.flaticon.com/authors/mynamepong" title="mynamepong">mynamepong</a> and <a href="https://www.flaticon.com/authors/gregor-cresnar" title="Gregor Cresnar">Gregor Cresnar</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>.</p>
 </div>
 </template>
 
