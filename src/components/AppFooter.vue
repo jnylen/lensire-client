7b0ee97d1f83f950b7f2091f292a23090d43840e
Joakim Nylén <me@jnylen.nu>
2018-07-13 09:55:36 +0200

    Add disclaimer to the footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,6 +6,12 @@ export default {
 
 <template>
 <div class="p-6">
+  <div class="w-full text-center pb-8">
+    <router-link to="/impressum" class="text-grey-darkest text-sm font-normal hover:no-underline">
+      <span class="font-semibold">Disclaimer:</span><br/>
+      Some of the links are "affiliate links", a link with a special tracking code. This means if you click on an affiliate link and purchase the item, we will receive an affiliate commission.
+    </router-link>
+  </div>
   <div class="flex">
     <div class="justify-start text-left w-1/3 text-grey-dark text-sm font-normal">
       <a class="font-semibold text-grey-dark">Lensire</a> by <a class="font-semibold text-grey-dark">Meiku</a>
